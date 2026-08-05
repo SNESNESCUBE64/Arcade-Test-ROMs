ee3602c1c46810010350a843bb3bd4f995335e1c
SNESNESCUBE64 <schalkmatthew@gmail.com>
2026-08-04 22:26:34 -0400

    Updated build script to work properly

--- Tools/Build Scripts/TKGBuild.bat
+++ Tools/Build Scripts/TKGBuild.bat
@@ -1,1 +1,6 @@
-z80asm -I=..\..\Source -I=..\..\Public -mz80 -b -O=..\..\Build TKGMain.asm -o=TkgTest5E.bin
+@echo off
+cd ..\..\
+z80asm -I=Source -I=Public -mz80 -b -O=.\Build TKGMain.asm -o=TKGTestRom.bin
+python ".\Tools\Checksum Patch Utility\ChecksumPatchUtility.py" ".\Build\TKGTestRom.bin"
+move .\Build\Source\* .\Build > nul
+rmdir .\Build\Source
