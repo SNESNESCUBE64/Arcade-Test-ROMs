ba641c00401c634c19e826ca1d8895235334fa6b
SNESNESCUBE64 <schalkmatthew@gmail.com>
2026-08-06 23:57:42 -0400

    Added Runtime Test header

--- Code/Source/TKGRuntime.asm
+++ Code/Source/TKGRuntime.asm
@@ -4,7 +4,11 @@
 TKGRuntime_Main:
 ;temporary until the menu can be written out.
     ;call audio_test_main
+    ld de, tkg_header_address
+    ld hl, string_tkg_runtime
+    call print
+runtime_loop:    
     call runtime_controls_main
-    jr TKGRuntime_Main
+    jr runtime_loop
 
 include "TKGControls.asm"
