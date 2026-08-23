5d6c1250f42defff5dda55c06639c8d262a3f15f
SNESNESCUBE64 <schalkmatthew@gmail.com>
2026-08-22 20:18:46 -0400

    Changed the default fill value to $C7

--- Tools/Build Scripts/TKGBuild.bat
+++ Tools/Build Scripts/TKGBuild.bat
@@ -1,6 +1,6 @@
 @echo off
 cd ..\..\
-z80asm -I=Code\Source -I=Code\Public -mz80 -b -O=.\Build TKGMain.asm -o=TKGTestRom.bin
+z80asm -I=Code\Source -I=Code\Public -mz80 -b -O=.\Build TKGMain.asm -o=TKGTestRom.bin -f=$c7
 python ".\Tools\Checksum Patch Utility\ChecksumPatchUtility.py" ".\Build\TKGTestRom.bin"
 move .\Build\Code\Source\* .\Build\Code > nul
 rmdir .\Build\Code\Source
